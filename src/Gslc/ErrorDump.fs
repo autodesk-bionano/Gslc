ad16b51c5e45adacdd816904c44aec3bfc40a1dc
Rupal Khilari <rupal.khilari@autodesk.com>
2016-11-30 11:49:17 -0800

    Dump error info into a json file.

---
+++ src/Gslc/ErrorDump.fs
@@ -0,0 +1,31 @@
+﻿module ErrorDump
+
+open System
+open System.IO
+open System.Text
+open commonTypes
+open constants
+open Amyris.Bio.utils
+open shared
+open Newtonsoft.Json
+open System.Collections.Generic
+open PluginTypes
+open CoreOutputProviders    
+open AstAlgorithms
+open Amyris.ErrorHandling
+open AstErrorHandling
+
+
+type AutodeskErrorDumpOutputProvider (outPath: (string) option) =
+    inherit ConfigurableOutputProvider<string>(outPath)
+    with
+    override x.ArgSpec =
+        {name = "errordump"; param = ["outfile"]; alias = [];
+         desc = "dump out error hash in json format in the specified"}
+    override x.UseArg(arg) =
+        AutodeskErrorDumpOutputProvider(Some(arg.values.[0]))
+        :> IOutputFormat
+    override x.DoOutput(path, data) = printfn "%s" path // Dummy method to print out the path.
+ 
+let autodeskErrorDumpOutputPlugin = outputPlugin "Autodesk error output file format" (AutodeskErrorDumpOutputProvider(None))
+
--- src/Gslc/Program.fs
+++ src/Gslc/Program.fs
@@ -1,5 +1,6 @@
 ﻿/// top-level entry point for GSLc.
 /// Injects plugins into main compiler function call.
+open System.IO
 open gslc
 open CoreOutputProviders
 open JsonAssembly
@@ -7,18 +8,93 @@ open BasicCodonProvider
 open BasicAlleleSwapProvider
 open BasicL2ExpansionProvider
 open utils
+open commonTypes
+open commandConfig
+open LexAndParse
+open gslcProcess // Top-level compiler operations
+open AstAlgorithms
+open Amyris.ErrorHandling
+open AstErrorHandling
+// Helper libs for oligo design, sequence parsing all in Amyris.Bio.dll
+// These imports are only needed for the temporary primer test function below.
+open Amyris.Bio
+open primercore
+open utils
+open constants
+open PluginTypes
+open AstTypes
+open ProcessCmdLineArgs
+open Newtonsoft.Json
+open ErrorDump
 
 let allPlugins = 
     basicOutputPlugins@
     [autodeskJsonOutputPlugin;
+     autodeskErrorDumpOutputPlugin;
      basicCodonProviderPlugin;
      basicAlleleSwapPlugin;
-     basicL2ExpansionPlugin]
+     basicL2ExpansionPlugin;]
+
+type ErrorJson =
+    {startPosition: string;
+      endPosition: string;
+      message: string;
+      stackTrace: string;
+      summary: string;
+}
+
+let dumpErrorInfo (errors: AstMessage list, outFile: string) =
+    use outF = new StreamWriter(outFile)
+    let errorHash = 
+        errors
+        |> List.map(fun a->
+            {startPosition = (match a.sourcePosition with | None -> "" | Some postion -> postion.ToString()); // TODO: format this.
+            endPosition = (match a.sourcePosition with | None -> "" | Some postion -> postion.ToString()); // TODO: format this.
+            message = a.msg.ToString();
+            stackTrace = (match a.stackTrace with | None -> "" | Some st -> st.ToString());
+            summary = a.Summary.ToString();
+        })
+
+    let errorHashSerialized:string = Newtonsoft.Json.JsonConvert.SerializeObject(errorHash, Formatting.Indented)
+    outF.WriteLine(errorHashSerialized)
+
+
+let handleCompileResultAndErrors (result, input: GslSourceCode, s) =
+    match result with
+    | Ok((assemblies, tree: AstTreeHead), warnings) ->
+        // print any warnings from compilation
+        for w in deduplicateMessages warnings do printfn "%s\n" w.Summary
+        // if we just want one expansion step, reprint expanded source and done
+        if not s.opts.iter || s.opts.onlyPhase1 then
+            Exit(0, Some(decompile tree.wrappedNode))
+        // do output generation
+        else
+            Continue(assemblies, s)
+    | Bad(errors) ->
+        // convert messages into strings for printing
+        let msgs = [for msg in deduplicateMessages errors -> msg.Longform(s.opts.verbose, input.String)]
+        // TODO: Dump this into a path specified by the --errordump flag instead of /tmp
+        dumpErrorInfo(errors, "/tmp/errordump.json")
+        Exit(1, Some(msgs |> String.concat "\n\n"))
+
+
+/// Main function call to run GSLc from the command line.
+/// Clients of GSLCore may wish to bypass this function and write their own version if they want
+/// to intercept the output from the various compiler stages.
+let gslcCustom unconfiguredPlugins argv : FlowControl<_> =
+
+    // Configure GSLc and plugins from command line arguments
+    configureGslc unconfiguredPlugins argv
+    >?> runCompiler
+    >?> handleCompileResultAndErrors
+    >?> doAssemblyTransform
+    >?> handleTransformResult
+    >?> doOutput
 
 [<EntryPoint>]
 let main argv =
     try
-        let flowResult = gslc allPlugins argv
+        let flowResult = gslcCustom allPlugins argv
         match flowResult with
         | Exit(code, msg) ->
             msg |> Option.map (printf "%s") |> ignore
